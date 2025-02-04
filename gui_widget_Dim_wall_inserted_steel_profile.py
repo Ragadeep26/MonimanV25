7ddf8f5e9a3216e7208d87d48fc4817b12f2f6e9
Ragadeep Bojja <80882218+Ragadeep26@users.noreply.github.com>
2025-02-04 11:32:54 +0100

    Dry files

---
+++ gui_widget_Dim_wall_inserted_steel_profile.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui_widget_Dim_wall_inserted_steel_profile.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
